vinylspiders/tgstation

commit 940d91bb62469f0fd8a9c57b11ddb2d03100c715

[MDB IGNORE] Adds new features and tiles for lavaland biomes (#95955)
## About The Pull Request

Adds new floor types and natural decor to both Siderite and Shale
biomes, as well as makes flora and fauna spawns differ between said
biomes.

<img width="1216" height="997" alt="dreamseeker_uIv1OgVkpD"
src="https://github.com/user-attachments/assets/327d9caa-a58c-4d70-acbe-477da69b4d71"
/>

---

Basalt biome now spawns more legions and goliaths, but less watchers and
no brimdemons. You can rarely find new volcanic pore rocks which will
spew out lava when destroyed.

<img width="505" height="361" alt="dreamseeker_TA9rXzOgad"
src="https://github.com/user-attachments/assets/fb19a60a-c927-441b-abad-161ce5022a60"
/>


Siderite biome has a lot of rock formations and may sometimes spawn new
stalagmites. You can expect to see a ton of bileworms, raptors and
watchers there.


<img width="747" height="613" alt="dreamseeker_uZgRX8qqh3"
src="https://github.com/user-attachments/assets/d356f1c6-371a-4b8b-899b-470edb0d2790"
/>


Shale is full of lobstrocities, goldgrubs and brimdemons, and home to a
new glowgrowth "plant" - colony of bioluminescent fungi growing on
natural air vents.

<img width="274" height="220" alt="dreamseeker_EtxiS4X9M7"
src="https://github.com/user-attachments/assets/cbefba67-b618-4a65-ad12-9ee5182b650c"
/>


While it cannot be planted by itself, it contains luminescent fluid
inside which will grant whoever eats it minor night vision as long as it
is inside their bloodstream (as well as cyan-glowing eyes)

---

Ruins now properly respect their biome's rock (and now floor as well)
type, since it was broken for some of them previously.

<details>
<summary>More images under the dropdown</summary>

<img width="1216" height="997" alt="dreamseeker_fpXXV0Rt3D"
src="https://github.com/user-attachments/assets/fa790ca1-7045-490e-bcb2-c1f9da9f277c"
/>
<img width="414" height="303" alt="dreamseeker_l6E8S2fzCe"

_maps/RandomRuins/LavaRuins/lavaland_surface_hermit.dmm
@@ -10,10 +10,10 @@
 /area/ruin/powered)
 "e" = (
 /obj/item/clothing/suit/space/orange,
-/turf/open/misc/asteroid/basalt,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered)
 "f" = (
-/turf/open/misc/asteroid/basalt,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered)
 "g" = (
 /turf/open/misc/asteroid{
@@ -57,7 +57,7 @@
 /obj/item/seeds/plump,
 /obj/item/food/grown/mushroom/glowshroom,
 /obj/item/seeds/tower,
-/turf/open/misc/asteroid/basalt,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered)
 "n" = (
 /obj/machinery/hydroponics/soil,
@@ -71,7 +71,7 @@
 /obj/item/storage/bag/plants/portaseeder,
 /obj/item/storage/bag/ore,
 /obj/item/storage/medkit/regular,
-/turf/open/misc/asteroid/basalt,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered)
 "q" = (
 /obj/structure/glowshroom/single,
@@ -81,10 +81,10 @@
 /obj/structure/rack,
 /obj/item/pickaxe/emergency,
 /obj/item/tank/internals/oxygen,
-/turf/open/misc/asteroid/basalt,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered)
 "s" = (
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "t" = (
 /turf/closed/wall/mineral/titanium/survival/pod,
@@ -174,7 +174,7 @@
 /area/ruin/powered)
 "M" = (
 /obj/machinery/portable_atmospherics/canister/oxygen,
-/turf/open/misc/asteroid/basalt,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered)
 "P" = (
 /turf/template_noop,

code/game/turfs/open/asteroid.dm
@@ -186,9 +186,34 @@ GLOBAL_LIST_EMPTY(dug_up_basalt)
 /turf/open/misc/asteroid/basalt/lava_land_surface/no_ruins
 	turf_flags = NO_RUINS
 
-/// A turf that can't we can't build openspace chasms on or spawn ruins in.
-/turf/closed/mineral/volcanic/lava_land_surface/do_not_chasm
-	turf_flags = NO_RUINS
+/// Variant for ruins which replaces itself with the floor of a biome it generates in
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace
+	icon = 'icons/turf/mining.dmi'
+	icon_state = "basalt_biome"
+	smoothing_flags = NONE
+	/// Area type to whose generator we try to default to
+	var/default_area = /area/lavaland/surface/outdoors/unexplored/danger
+
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/Initialize(mapload)
+	. = ..()
+	// Just spawn a normal lavaland rock if we fail to get a mapgen, such as being spawned over lava
+	var/supposed_type = /turf/open/misc/asteroid/basalt/lava_land_surface
+	var/area/cur_area = loc
+	var/datum/map_generator/cave_generator/map_generator = cur_area?.get_generator()
+	if (!map_generator)
+		cur_area = GLOB.areas_by_type[default_area]
+		map_generator = cur_area?.get_generator()
+
+	if (istype(map_generator))
+		var/biome = map_generator.get_biome_for_turf(src)
+		if (biome)
+			var/datum/biome/generating_biome = SSmapping.biomes[biome]
+			supposed_type = generating_biome.open_turf_type
+
+	var/cur_flags = turf_flags
+	var/turf/new_turf = ChangeTurf(supposed_type, flags = CHANGETURF_FORCEOP | CHANGETURF_INHERIT_AIR)
+	if(cur_flags & NO_RUINS)
+		new_turf.turf_flags |= NO_RUINS
 
 /turf/open/misc/asteroid/lowpressure
 	initial_gas_mix = OPENTURF_LOW_PRESSURE
@@ -200,6 +225,77 @@ GLOBAL_LIST_EMPTY(dug_up_basalt)
 	baseturfs = /turf/open/misc/asteroid/airless
 	turf_type = /turf/open/misc/asteroid/airless
 	worm_chance = 0
+
+/turf/open/misc/asteroid/basalt/smooth
+	smoothing_flags = SMOOTH_BITMASK
+	layer = MID_TURF_LAYER
+	floor_variance = 0
+	transform = MAP_SWITCH(TRANSLATE_MATRIX(-8, -8), matrix())
+	/// DMI used by unsmoothed turfs for variance
+	var/variant_dmi = null
+	/// Amount of variants this turf has
+	var/variant_num = 8
+
+/turf/open/misc/asteroid/basalt/smooth/set_smoothed_icon_state(new_junction)
+	. = ..()
+	if (new_junction == 255 && variant_dmi)
+		icon = variant_dmi
+		icon_state = "[base_icon_state][rand(1, variant_num)]"
+	else
+		icon = initial(icon)
+
+/turf/open/misc/asteroid/basalt/smooth/update_overlays()
+	. = ..()
+	if (smoothing_junction != 255 && variant_dmi)
+		. = list(mutable_appearance(variant_dmi, "[base_icon_state][rand(1, variant_num)]")) + .
+
+/turf/open/misc/asteroid/basalt/smooth/get_smooth_underlay_icon(mutable_appearance/underlay_appearance, turf/asking_turf, adjacency_dir)
+	. = ..()
+	if (!.)
+		return
+	if(!smoothing_flags)
+		return
+	underlay_appearance.transform = transform
+
+/turf/open/misc/asteroid/basalt/smooth/siderite
+	name = "siderite floor"
+	baseturfs = /turf/open/misc/asteroid/basalt/smooth/siderite
+	icon = 'icons/turf/floors/siderite.dmi'
+	damaged_dmi = 'icons/turf/floors/siderite_variants.dmi'
+	variant_dmi = 'icons/turf/floors/siderite_variants.dmi'
+	icon_state = "siderite-255"
+	base_icon_state = "siderite"
+	layer = HIGH_TURF_LAYER
+	smoothing_groups = SMOOTH_GROUP_TURF_OPEN + SMOOTH_GROUP_FLOOR_SIDERITE
+	canSmoothWith = SMOOTH_GROUP_FLOOR_SIDERITE + SMOOTH_GROUP_CLOSED_TURFS
+	dig_result = /obj/item/stack/ore/glass/siderite
+
+/turf/open/misc/asteroid/basalt/smooth/siderite/lava_land_surface
+	initial_gas_mix = LAVALAND_DEFAULT_ATMOS
+	planetary_atmos = TRUE
+	baseturfs = /turf/open/lava/smooth/lava_land_surface
+
+/turf/open/misc/asteroid/basalt/smooth/siderite/lava_land_surface/no_ruins
+	turf_flags = NO_RUINS
+
+/turf/open/misc/asteroid/basalt/smooth/shale
+	name = "shale floor"
+	baseturfs = /turf/open/misc/asteroid/basalt/smooth/shale
+	icon = 'icons/turf/floors/shale.dmi'
+	damaged_dmi = 'icons/turf/floors/shale_variants.dmi'
+	variant_dmi = 'icons/turf/floors/shale_variants.dmi'
+	icon_state = "shale-255"
+	base_icon_state = "shale"
+	smoothing_groups = SMOOTH_GROUP_TURF_OPEN + SMOOTH_GROUP_FLOOR_SHALE
+	canSmoothWith = SMOOTH_GROUP_FLOOR_SHALE + SMOOTH_GROUP_CLOSED_TURFS
+
+/turf/open/misc/asteroid/basalt/smooth/shale/lava_land_surface
+	initial_gas_mix = LAVALAND_DEFAULT_ATMOS
+	planetary_atmos = TRUE
+	baseturfs = /turf/open/lava/smooth/lava_land_surface
+
+/turf/open/misc/asteroid/basalt/smooth/shale/lava_land_surface/no_ruins
+	turf_flags = NO_RUINS
 
 /turf/open/misc/asteroid/snow
 	gender = PLURAL

_maps/RandomRuins/LavaRuins/lavaland_battle_site.dmm
@@ -10,7 +10,7 @@
 	desc = "A crown that was fit for a king, looks like it didn't get them very far.";
 	name = "dented crown"
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "g" = (
 /obj/structure/water_source/puddle,
@@ -19,18 +19,18 @@
 "h" = (
 /obj/effect/mob_spawn/corpse/human/skeleton,
 /obj/item/shield/buckler,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "j" = (
 /turf/template_noop,
 /area/template_noop)
 "m" = (
 /obj/effect/decal/cleanable/shreds,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "n" = (
 /obj/structure/stone_tile/block/burnt,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "o" = (
 /obj/structure/stone_tile/burnt{
@@ -41,39 +41,39 @@
 	},
 /obj/effect/decal/cleanable/blood/drip,
 /mob/living/basic/mining/goliath,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "p" = (
 /obj/item/stack/rods{
 	pixel_x = 10
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "q" = (
 /obj/structure/flora/rock,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "s" = (
 /mob/living/basic/mining/goliath,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "t" = (
 /obj/effect/decal/cleanable/blood/gibs/old,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "u" = (
 /obj/effect/decal/cleanable/blood/old,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "x" = (
 /obj/effect/decal/cleanable/blood/drip,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "D" = (
 /obj/structure/statue/bone/rib{
 	name = "colossal tailbone"
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "F" = (
 /obj/effect/decal/cleanable/blood/drip,
@@ -81,33 +81,33 @@
 	dir = 1
 	},
 /obj/structure/statue/bone/rib,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "G" = (
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "H" = (
 /obj/structure/firepit,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "I" = (
 /obj/structure/closet/crate/wooden,
 /obj/item/stack/sheet/animalhide/goliath_hide,
 /obj/item/flashlight/flare/torch,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "J" = (
 /obj/structure/flora/ash/cap_shroom,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "K" = (
 /obj/structure/closet/crate/wooden,
 /obj/item/stack/sheet/sinew,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "M" = (
 /obj/structure/statue/bone/skull,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "O" = (
 /obj/structure/statue/bone/rib{
@@ -116,18 +116,18 @@
 /obj/structure/stone_tile/cracked{
 	dir = 4
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "P" = (
 /obj/effect/mob_spawn/corpse/human/skeleton,
 /obj/item/shovel/spade,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "S" = (
 /obj/structure/chair/wood/wings{
 	color = "#ffff00"
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "T" = (
 /obj/effect/mob_spawn/corpse/human/skeleton,
@@ -135,14 +135,14 @@
 	pixel_x = 13;
 	pixel_y = 12
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "U" = (
 /obj/structure/statue/bone/rib,
 /obj/structure/stone_tile/cracked{
 	dir = 1
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "V" = (
 /obj/effect/mob_spawn/corpse/human/skeleton,
@@ -150,14 +150,14 @@
 	pixel_x = -1;
 	pixel_y = -7
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "X" = (
 /obj/structure/stone_tile/burnt,
 /obj/structure/stone_tile/burnt{
 	dir = 4
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 "Y" = (
 /obj/effect/mob_spawn/corpse/human/skeleton,
@@ -165,7 +165,7 @@
 	pixel_y = -12;
 	pixel_x = 2
 	},
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors)
 
 (1,1,1) = {"

_maps/RandomRuins/LavaRuins/lavaland_biodome_beach.dmm
@@ -260,7 +260,7 @@
 /area/ruin/powered/beach)
 "gX" = (
 /obj/machinery/atmospherics/components/unary/passive_vent/layer4,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors/explored)
 "hj" = (
 /obj/structure/flora/tree/palm,
@@ -630,7 +630,7 @@
 /turf/closed/wall/mineral/wood/nonmetal,
 /area/ruin/powered/beach)
 "wL" = (
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors/explored)
 "wY" = (
 /turf/open/floor/pod/light,

_maps/RandomRuins/LavaRuins/lavaland_biodome_clown_planet.dmm
@@ -441,7 +441,7 @@
 	name = "stealth banana peel"
 	},
 /obj/effect/mapping_helpers/no_lava,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors/explored)
 "jw" = (
 /obj/structure/disposalpipe/segment{
@@ -533,6 +533,11 @@
 /obj/effect/turf_decal/tile/yellow/fourcorners,
 /turf/open/indestructible/permalube,
 /area/ruin/powered/clownplanet)
+"qO" = (
+/obj/machinery/light/directional/west,
+/obj/effect/mapping_helpers/no_lava,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/area/ruin/powered/clownplanet)
 "rP" = (
 /obj/structure/disposalpipe/segment{
 	dir = 4;
@@ -563,6 +568,10 @@
 "ye" = (
 /turf/open/lava/smooth,
 /area/ruin/powered/clownplanet)
+"Am" = (
+/obj/effect/mapping_helpers/no_lava,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/area/lavaland/surface/outdoors/explored)
 "BO" = (
 /obj/structure/disposalpipe/segment{
 	dir = 4;
@@ -614,6 +623,11 @@
 	},
 /obj/effect/turf_decal/tile/yellow/fourcorners,
 /turf/open/indestructible/permalube,
+/area/ruin/powered/clownplanet)
+"Ig" = (
+/obj/machinery/light/directional/east,
+/obj/effect/mapping_helpers/no_lava,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/ruin/powered/clownplanet)
 "JU" = (
 /obj/item/bikehorn,
@@ -739,11 +753,11 @@ aa
 aa
 aa
 aa
-QT
-QT
-QT
-QT
-QT
+Am
+Am
+Am
+Am
+Am
 aa
 aa
 aa
@@ -772,11 +786,11 @@ cc
 cc
 cc
 cc
-Or
-QT
+Ig
+Am
 gX
-QT
-QT
+Am
+Am
 gX
 Or
 cc
@@ -1724,11 +1738,11 @@ cc
 cc
 cc
 cc
-Zg
-QT
+qO
+Am
 gX
-QT
-QT
+Am
+Am
 gX
 Zg
 cc
@@ -1759,11 +1773,11 @@ aa
 aa
 aa
 aa
-QT
-QT
-QT
-QT
-QT
+Am
+Am
+Am
+Am
+Am
 aa
 aa
 aa

_maps/RandomRuins/LavaRuins/lavaland_strong_rock.dmm
@@ -1,7 +1,7 @@
 //MAP CONVERTED BY dmm2tgm.py THIS HEADER COMMENT PREVENTS RECONVERSION, DO NOT REMOVE
 "a" = (
 /obj/effect/mapping_helpers/no_lava,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/template_noop)
 "b" = (
 /turf/closed/mineral/strong,

_maps/RandomRuins/LavaRuins/lavaland_surface_biodome_winter.dmm
@@ -302,7 +302,7 @@
 /area/ruin/powered/snow_biodome)
 "tb" = (
 /obj/effect/mapping_helpers/no_lava,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors/explored)
 "tl" = (
 /turf/open/floor/pod/light,
@@ -422,10 +422,10 @@
 "Qa" = (
 /obj/effect/mapping_helpers/no_lava,
 /obj/effect/mapping_helpers/no_lava,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors/explored)
 "QI" = (
-/turf/open/misc/asteroid/basalt/lava_land_surface,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
 /area/lavaland/surface/outdoors/explored)
 "QK" = (
 /obj/structure/table,

commit 299fc82b54513be06360df04d21d16a4150dc315

Fixes atmos diffs and incorrect atmos in lavaland hermit ruin (#96973)
## About The Pull Request
Caused by #95955, solved by adding a subtype of biome replace turf that
has standard atmos

## Changelog
:cl:
fix: Fixed atmos diffs and incorrect atmos in lavaland hermit ruin
/:cl:

_maps/RandomRuins/LavaRuins/lavaland_surface_hermit.dmm
@@ -10,10 +10,10 @@
 /area/ruin/powered)
 "e" = (
 /obj/item/clothing/suit/space/orange,
-/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos,
 /area/ruin/powered)
 "f" = (
-/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos,
 /area/ruin/powered)
 "g" = (
 /turf/open/misc/asteroid{
@@ -57,7 +57,7 @@
 /obj/item/seeds/plump,
 /obj/item/food/grown/mushroom/glowshroom,
 /obj/item/seeds/tower,
-/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos,
 /area/ruin/powered)
 "n" = (
 /obj/machinery/hydroponics/soil,
@@ -71,7 +71,7 @@
 /obj/item/storage/bag/plants/portaseeder,
 /obj/item/storage/bag/ore,
 /obj/item/storage/medkit/regular,
-/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos,
 /area/ruin/powered)
 "q" = (
 /obj/structure/glowshroom/single,
@@ -81,7 +81,7 @@
 /obj/structure/rack,
 /obj/item/pickaxe/emergency,
 /obj/item/tank/internals/oxygen,
-/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos,
 /area/ruin/powered)
 "s" = (
 /turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
@@ -174,7 +174,7 @@
 /area/ruin/powered)
 "M" = (
 /obj/machinery/portable_atmospherics/canister/oxygen,
-/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace,
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos,
 /area/ruin/powered)
 "P" = (
 /turf/template_noop,

code/game/turfs/open/asteroid.dm
@@ -217,6 +217,10 @@ GLOBAL_LIST_EMPTY(dug_up_basalt)
 	if(cur_flags & NO_RUINS)
 		new_turf.turf_flags |= NO_RUINS
 
+/turf/open/misc/asteroid/basalt/lava_land_surface/biome_replace/normal_atmos
+	initial_gas_mix = OPENTURF_DEFAULT_ATMOS
+	planetary_atmos = FALSE
+
 /turf/open/misc/asteroid/lowpressure
 	initial_gas_mix = OPENTURF_LOW_PRESSURE
 	baseturfs = /turf/open/misc/asteroid/lowpressure